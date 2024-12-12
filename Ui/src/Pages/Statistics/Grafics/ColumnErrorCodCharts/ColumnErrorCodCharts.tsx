2a14a464685e9dfcd23c6d174aa2d53e98e1e553
zero <artemlysakov793@gmail.com>
2024-12-12 11:20:05 +0300

    add descripteon

--- Ui/src/Pages/Statistics/Grafics/ColumnErrorCodCharts/ColumnErrorCodCharts.tsx
+++ Ui/src/Pages/Statistics/Grafics/ColumnErrorCodCharts/ColumnErrorCodCharts.tsx
@@ -6,16 +6,13 @@ interface Props {
   url: string;
 }
 
-interface DataItemError {
-  timestamp: string;
-  level: string;
-  message: string;
-  feeder: string;
-  head: string;
+interface DataItem {
+  x: string;
+  y: number;
 }
 
 export default function ColumnErrorCodCharts(props: Props) {
-  const dataArr = [
+  const dataArr: DataItem[] = [
     { x: 'af1a', y: 100},
     { x: 'ff2q', y: 70},
     { x: '2040', y: 30},
@@ -32,12 +29,26 @@ export default function ColumnErrorCodCharts(props: Props) {
     { x: '2042', y: 30},
     { x: '23d3', y: 8},
     { x: '0014', y: 20},
+    { x: '0011', y: 200},
     { x: '2025', y: 3}
   ];
 
-  const [selectedData, setSelectedData] = useState(dataArr);
-  const [limit, setLimit] = useState(10);
-  const [sortOrder, setSortOrder] = useState('asc');
+  const errorDescriptions: Record<string, string> = {
+    "af1a" : 'Ошибка сборки',
+    "ff2q" : 'Ошибка монтажа',
+    '2040' : 'Ошибка системы',
+    '23da' : 'Ошибка датчика',
+    '0010' : 'Ошибка связи',
+    '2021' : 'Ошибка программного обеспечения',
+    '2042' : 'Ошибка механизма',
+    '23d3' : 'Ошибка электроники',
+    '0014' : 'Ошибка управления',
+    '2025' : 'Ошибка системы безопасности'
+  };
+
+  const [selectedData, setSelectedData] = useState<DataItem[]>([]);
+  const [limit, setLimit] = useState<number>(10);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc');
 
   useEffect(() => {
     updateSelectedData();
@@ -73,9 +84,28 @@ export default function ColumnErrorCodCharts(props: Props) {
   const toggleSortOrder = () => {
     setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
   };
-
+  
   const options = {
+    tooltip: {
+      custom: function({series, seriesIndex, dataPointIndex, w}) {
+        const errorCode = selectedData[dataPointIndex].x;
+        const count = selectedData[dataPointIndex].y;
+        const description = errorDescriptions[errorCode] || 'Неизвестная ошибка';
+        
+        return '<div class="arrow_box">' +
+          `<span>${errorCode} (${count}) ${description}</span>` +
+          '</div>'
+      }
+    },
 
+    xaxis: {
+      categories: selectedData.map(item => item.x), // Используем данные из dataArr для категорий оси X
+      labels: {
+        formatter: function(val: string) {
+          return val; // Возвращаем значение как есть
+        }
+      }
+    },
   };
 
   return (
@@ -99,7 +129,7 @@ export default function ColumnErrorCodCharts(props: Props) {
       </div>
       <Chart 
         options={options} 
-        series={[{ name: 'Количество', data: selectedData }]} 
+        series={[{ name: 'Количество', data: selectedData.map(item => item.y) }]} 
         type="bar" 
         height={350} 
       />
